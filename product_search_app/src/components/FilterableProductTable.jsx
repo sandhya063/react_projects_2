cdadc463cb2d01e2db51adbc256678dd2945c990
sandhya063 <165256047+sandhya063@users.noreply.github.com>
2024-08-22 20:54:29 +1000

    made chnages in searchbar

--- product_search_app/src/components/FilterableProductTable.jsx
+++ product_search_app/src/components/FilterableProductTable.jsx
@@ -1,11 +1,36 @@
 import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function FilterableProductTable() {
+    const [inStock, setInStock] = useState(false);
+    function handleCheckBox(event){
+        const isChecked = event.target.checked;
+        setInStock(isChecked);
+        console.log(inStock);
+
+    }
+    function handleSearch(event) {
+        console.log(event.target.value);
+    }
+
+
+    const products = [
+        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }  
+
+      ];
+      const filteredProducts = products
+      .filter(product => inStock ? product.stocked: true);
+      .filter(product => product.name.toLowerCase().startsWith(query.to.LowerCase()));
     return (
         <div className="w-96 border">
-            <SearchBar />
-            <ProductTable />
+            <SearchBar handleCheckBox={handleCheckBox} handleSearch={handleSearch} />
+            <ProductTable products={filteredProducts}/>
         </div>
     )
 }
--- product_search_app/src/components/ProductTable.jsx
+++ product_search_app/src/components/ProductTable.jsx
@@ -1,22 +1,47 @@
 import ProductCategoryRow from "./ProductCategoryRow";
 import ProductRow from "./ProductRow";
+import PropTypes from 'prop-types';
+import React from "react";
 
-function ProductTable() {
+function ProductTable({products}) {
+    console.log(products);
+    const categories =  [];
+    products.forEach((product) => {
+        const category = product.category;
+        if (categories.indexOf(category) === -1) {
+            categories.push(category);
+        }
+
+    });
+    console.log(categories);
     return (
         <table className="w-full">
+            <tbody>
             <tr>
                 <th className="p-2">Name</th>
                 <th className="p-2">Price</th>
-            </tr>
-            <ProductCategoryRow category="Fruits" />
-            <ProductRow name="Apple" price="$1" stocked={true} />
-            <ProductRow name="Apple" price="$1" stocked={false} />
-            <ProductRow name="Apple" price="$1" stocked={false} />
-            <ProductCategoryRow category="Vegetables" />
-            <ProductRow name="Apple" price="$1" stocked={false} />
-            <ProductRow name="Apple" price="$1" stocked={false} />
-            <ProductRow name="Apple" price="$1" stocked={false} />
+                </tr>
+                {
+                     
+        categories.map((category, index) => (
+           <React.Fragment key={index}>
+            <ProductCategoryRow
+             category={category} />
+            {
+                products.filter(product => product.category === category)
+                .map((product, index) => 
+                    <ProductRow
+                 key={index}
+                  name={product.name} 
+                  price={product.price}
+                   stocked={product.stocked} />)
+            }
+            </React.Fragment>
+        ))
+    }
+    </tbody> 
         </table>
     )
 }
+
 export default ProductTable;
